14cd16e8f06d461fbd3956774501dec8bf981ee7
Aaron Abebe <aaron.abebe@gmail.com>
2024-03-19 18:10:07 +0100

    feat: support for musicgen training/finetuning based on audiocraft (#7)

--- src/nendo_plugin_musicgen/plugin.py
+++ src/nendo_plugin_musicgen/plugin.py
@@ -1,13 +1,16 @@
+import os
+import time
+import yaml
+import torch
+import subprocess
 from logging import Logger
 from typing import Any, Optional, Literal
-
+from tqdm import tqdm
+import json
+import audiocraft
 from audiocraft.models import MusicGen
-from nendo import (
-    Nendo,
-    NendoConfig,
-    NendoGeneratePlugin,
-    NendoTrack,
-)
+from audiocraft.utils import export
+from nendo import Nendo, NendoConfig, NendoGeneratePlugin, NendoTrack, NendoCollection
 
 from .config import NendoMusicGenConfig
 from .musicgen import load_model, do_predictions
@@ -52,8 +55,203 @@ class NendoMusicGen(NendoGeneratePlugin):
         super().__init__(**data)
         self.logger.info("Initializing plugin: MUSICGEN")
 
+    def train(
+        self,
+        collection: NendoCollection,
+        finetune: bool = False,
+        prompt: str = "",
+        model: str = settings.model,
+        epochs: int = settings.epochs,
+        updates_per_epoch: int = settings.updates_per_epoch,
+        batch_size: int = settings.batch_size,
+        lr: float = settings.lr,
+        lr_scheduler: str = settings.lr_scheduler,
+        warmup: int = settings.warmup_steps,
+        optimizer: str = "adamw",
+        cfg_p: float = settings.cfg_p,
+        output_dir: str = settings.train_output_dir,
+    ):
+        """Finetune a pretrained model on a collection of tracks.
+
+        Args:
+            collection (NendoCollection): Collection of tracks to finetune on.
+            model (Optional[str]): Model version to use.
+            epochs (Optional[int]): Number of epochs to finetune.
+            updates_per_epoch (Optional[int]): Number of updates per epoch.
+            batch_size (Optional[int]): Batch size.
+            lr (Optional[float]): Learning rate.
+            lr_scheduler (Optional[str]): Learning rate scheduler.
+            warmup_steps (Optional[int]): Number of warmup steps.
+            output_dir (Optional[str]): Output directory for the finetuned model.
+
+        Returns:
+            MusicGen: Finetuned model.
+        """
+        train_len = 0
+        max_sample_rate = settings.sample_rate
+        os.makedirs(output_dir, exist_ok=True)
+
+        file_content = ""
+        for track in tqdm(collection.tracks()):
+            classify_data = track.get_plugin_data("nendo_plugin_classify_core")
+
+            if len(classify_data) < 1:
+                self.logger.warning(
+                    f"""
+                No classification data found for track {track.id}!
+                For good finetuning results we recommend running `nendo_plugin_classify_core` first.
+                Otherwise conditioning the model will be worse.
+                """
+                )
+
+            def check_get_data(key):
+                data = track.get_plugin_data("nendo_plugin_classify_core", key)
+                if len(data) < 1:
+                    return ""
+                return data[0].value
+
+            entry = {
+                "key": check_get_data("key"),
+                "artist": track.get_meta("artist") or "",
+                "sample_rate": track.sr,
+                "file_extension": "wav",
+                "description": prompt,
+                "keywords": "",
+                "duration": track.signal.shape[1] / track.sr,
+                "bpm": check_get_data("tempo"),
+                "genre": check_get_data("genre"),
+                "title": track.get_meta("title") or "",
+                "name": track.get_meta("name") or "",
+                "instrument": check_get_data("instrument"),
+                "moods": check_get_data("moods"),
+                "path": track.resource.src,
+            }
+            max_sample_rate = track.sr
+
+            train_len += 1
+            file_content += json.dumps(entry) + "\n"
+            track.__dict__["signal"] = None
+
+        with open(output_dir + "/data.jsonl", "a") as train_file:
+            train_file.write(file_content)
+
+        # replace default config in audiocraft install dir
+        pth = os.path.dirname(audiocraft.__file__)
+        default_cfg_path = os.path.join(pth, "../config/dset/audio/default.yaml")
+        with open(default_cfg_path, "r") as file:
+            data = yaml.safe_load(file)
+
+        data["datasource"]["max_sample_rate"] = max_sample_rate
+        data["datasource"]["max_channels"] = 2
+        data["datasource"]["train"] = output_dir
+        data["datasource"]["valid"] = output_dir
+        data["datasource"]["evaluate"] = output_dir
+        data["datasource"]["generate"] = output_dir
+
+        with open(default_cfg_path, "w") as file:
+            file.write("# @package __global__\n")
+            yaml.dump(data, file)
+
+        # replace dora default output dir in yaml
+        default_cfg_path = os.path.join(pth, "../config/teams/default.yaml")
+        with open(default_cfg_path, "r") as file:
+            data = yaml.safe_load(file)
+
+        data["default"]["dora_dir"] = output_dir
+
+        with open(default_cfg_path, "w") as file:
+            yaml.dump(data, file)
+
+        if model not in ["melody", "stereo-melody"]:
+            solver = "musicgen/musicgen_base_32khz"
+            model_scale = model.rsplit("-")[-1]
+            conditioner = "text2music"
+        else:
+            solver = "musicgen/musicgen_melody_32khz"
+            model_scale = "medium"
+            conditioner = "chroma2music"
+        continue_from = f"//pretrained/{model}"
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        args = [
+            "python",
+            "-m",
+            "dora",
+            "-P",
+            "audiocraft",
+            "run",
+            f"solver={solver}",
+            f"model/lm/model_scale={model_scale}",
+            f"conditioner={conditioner}",
+            f"device={device}",
+        ]
+        if finetune:
+            args.append(f"continue_from={continue_from}")
+        if "stereo" in model:
+            args.append(f"codebooks_pattern.delay.delays={[0, 0, 1, 1, 2, 2, 3, 3]}")
+            args.append("transformer_lm.n_q=8")
+            args.append("interleave_stereo_codebooks.use=True")
+            args.append("channels=2")
+
+        args.append(f"datasource.max_sample_rate={max_sample_rate}")
+        args.append(f"datasource.train={output_dir}")
+
+        # for quicker training we ignore validation and generation
+        args.append(f"dataset.valid.num_samples={1}")
+        args.append(f"dataset.generate.num_samples={1}")
+        args.append(f"dataset.train.num_samples={len(collection)}")
+
+        args.append(f"optim.epochs={epochs}")
+        args.append(f"optim.lr={lr}")
+        args.append(f"schedule.lr_scheduler={lr_scheduler}")
+        args.append(f"schedule.cosine.warmup={warmup}")
+        args.append(f"schedule.polynomial_decay.warmup={warmup}")
+        args.append(f"schedule.inverse_sqrt.warmup={warmup}")
+        args.append(f"schedule.linear_warmup.warmup={warmup}")
+        args.append(f"classifier_free_guidance.training_dropout={cfg_p}")
+
+        if updates_per_epoch is not None:
+            args.append(
+                f"logging.log_updates={updates_per_epoch // 10 if updates_per_epoch // 10 >= 1 else 1}"
+            )
+        else:
+            args.append(f"logging.log_updates=0")
+        args.append(f"dataset.batch_size={batch_size}")
+        args.append(f"optim.optimizer={optimizer}")
+
+        if updates_per_epoch is None:
+            args.append("dataset.train.permutation_on_files=False")
+            args.append("optim.updates_per_epoch=1")
+        else:
+            args.append("dataset.train.permutation_on_files=True")
+            args.append(f"optim.updates_per_epoch={updates_per_epoch}")
+
+        # call dora subprocess
+        self.logger.warning("Starting training with args:\n" + " ".join(args))
+        subprocess.check_output(args)
+
+        # export model after training
+        # get model checkpoint path
+        for dirpath, dirnames, filenames in os.walk(output_dir):
+            for filename in [f for f in filenames if f == "checkpoint.th"]:
+                checkpoint_dir = os.path.join(dirpath, filename)
+
+        print("Exporting model from checkpoint dir:", checkpoint_dir)
+
+        export.export_lm(checkpoint_dir, os.path.join(output_dir, "state_dict.bin"))
+        export.export_pretrained_compression_model(
+            "facebook/encodec_32khz",
+            os.path.join(output_dir, "compression_state_dict.bin"),
+        )
+        print("Run your new model using the nendo_plugin_musicgen plugin:")
+        print("from nendo import Nendo")
+        print("nd = Nendo(plugins=['nendo_plugin_musicgen'])")
+        print("generations = nd.plugins.musicgen(model='" + output_dir + "')")
+        return
+
     @NendoGeneratePlugin.run_track
-    def run_track(
+    def generate(
         self,
         track: Optional[NendoTrack] = None,
         bpm: int = 0,
@@ -154,9 +352,11 @@ class NendoMusicGen(NendoGeneratePlugin):
             scale=scale,
             sr_select=sr or settings.sample_rate,
             trim_start=start_time,
-            trim_end=duration
-            if use_melody_conditioning
-            else (duration or settings.duration) - conditioning_length,
+            trim_end=(
+                duration
+                if use_melody_conditioning
+                else (duration or settings.duration) - conditioning_length
+            ),
             overlay=conditioning_length,
             duration=duration,
             sample=musicgen_sample if not use_melody_conditioning else None,
